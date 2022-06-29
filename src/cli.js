62f92f3406277d47b6cea97072eecc2b9359c71a
dmitry zhigulin <jigulin.d@gmail.com>
2022-06-29 20:42:03 +0400

    Fix greeting logic

--- src/cli.js
+++ src/cli.js
@@ -5,6 +5,4 @@ export default () => {
 
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
-
-  return playerName;
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import readlineSync from 'readline-sync';
-import cli from './cli.js';
 import { roundsToWin } from './gameConstants.js';
 
-export default (instruction, generateRound) => {
-  const playerName = cli();
+export default (instruction, getQuestionAndAnswer) => {
+  console.log('Welcome to the Brain Games!');
+
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
 
   console.log(instruction);
 
   for (let i = 1; i <= roundsToWin; i += 1) {
-    const [question, expectedAnswer] = generateRound();
+    const [question, expectedAnswer] = getQuestionAndAnswer();
     console.log(`Question: ${question}`);
 
     const playerAnswer = readlineSync.question('Your Answer: ');
